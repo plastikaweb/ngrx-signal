d3fd26712078dec2d680bedc0327e1c9d0c077d1
Carlos Matheu Armengol <info@plastikaweb.com>
2024-07-13 11:09:05 +0200

    initial installation

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCwQEt3DFwLqJDszunvlnykAgELj2W-mdc",
+    authDomain: "plastik-ngrx-signal.firebaseapp.com",
+    projectId: "plastik-ngrx-signal",
+    storageBucket: "plastik-ngrx-signal.appspot.com",
+    messagingSenderId: "333273548473",
+    appId: "1:333273548473:web:824d6ae577ef8cc8d8fd68",
+    measurementId: "G-2DGQTZC2GK"
+  }
+};
